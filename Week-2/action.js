162448a1f8c1e32ba68295860031671eece02e1f
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-04 20:58:05 +0800

    Update action.js

--- Week-2/action.js
+++ Week-2/action.js
@@ -1,3 +1,10 @@
-document.querySelector('div.banner').addEventListener('click', () => {
-  document.querySelector('h1.headline').textContent = 'Have a Good Time!';
+const banner = document.querySelector('div.banner') ;
+const headline = document.querySelector('h1.headline') ;
+
+banner.addEventListener('click', () => {
+  if (headline.textContent == 'Meet the Quokka') {
+  headline.textContent = 'Have a Good Time!';
+  } else {
+  headline.textContent = 'Meet the Quokka';
+  }
 } );
