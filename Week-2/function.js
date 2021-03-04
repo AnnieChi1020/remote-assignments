64050894cc0a9305a565bcbd27262e771f83fb2d
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-04 20:49:58 +0800

    Update function.js

--- Week-2/function.js
+++ Week-2/function.js
@@ -1,9 +1,5 @@
-const headline = document.querySelector('h1.headline');
-const banner = document.querySelector('.banner');
-
-
-headline.addEventListener('click', () => {
-  headline.textContent = 'Have a Good Time!';
+document.querySelector('div.banner').addEventListener('click', () => {
+  document.querySelector('h1.headline').textContent = 'Have a Good Time!';
 } );
 
 function myFunction() {
