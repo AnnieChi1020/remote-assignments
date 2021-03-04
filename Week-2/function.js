47d775e231ef21bbaea35b36f6ede84d927ba133
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-04 20:51:59 +0800

    Update function.js

--- Week-2/function.js
+++ Week-2/function.js
@@ -1,7 +1,3 @@
-document.querySelector('div.banner').addEventListener('click', () => {
-  document.querySelector('h1.headline').textContent = 'Have a Good Time!';
-} );
-
 function myFunction() {
   var x = document.getElementById("myMainnav");
   if (x.className === "main-nav") {
