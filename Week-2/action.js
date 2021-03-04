57ac351f3ffb810a0914cc5241e0f22049b15a51
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-04 21:01:45 +0800

    Update action.js

--- Week-2/action.js
+++ Week-2/action.js
@@ -4,7 +4,7 @@ const headline = document.querySelector('h1.headline') ;
 banner.addEventListener('click', () => {
   if (headline.textContent == 'Meet the Quokka') {
   headline.textContent = 'Have a Good Time!';
-  } else {
+  } else if (headline.textContent == 'Have a Good Time!') {
   headline.textContent = 'Meet the Quokka';
   }
 } );
