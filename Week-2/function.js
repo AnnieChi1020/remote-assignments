e4a4a4d992c5034bda363d13ff64669af7791941
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-04 20:48:44 +0800

    Update function.js

--- Week-2/function.js
+++ Week-2/function.js
@@ -2,6 +2,10 @@ const headline = document.querySelector('h1.headline');
 const banner = document.querySelector('.banner');
 
 
+headline.addEventListener('click', () => {
+  headline.textContent = 'Have a Good Time!';
+} );
+
 function myFunction() {
   var x = document.getElementById("myMainnav");
   if (x.className === "main-nav") {
@@ -11,8 +15,5 @@ function myFunction() {
   }
 }
 
-headline.addEventListener('click', () => {
-  headline.textContent = 'Have a Good Time!';
-} );
 
 
