fb1679b61f10b47cf823821717e84f914f31fa8c
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-07 14:32:59 +0800

    Delete Week-2 directory

--- Week-2/js/action.js
+++
@@ -1,6 +0,0 @@
-const banner = document.querySelector('div.banner') ;
-const headline = document.querySelector('h1.headline') ;
-
-banner.addEventListener('click', () => {
-   headline.textContent = 'Have a Good Time!';
-} );
