d3588253e3af32a6f8d9b2db05b2578605f28a01
AnnieChi1020 <77234273+AnnieChi1020@users.noreply.github.com>
2021-03-07 22:52:12 +0800

    Update Assignment 2.js

--- Week-2/Assignment 2.js
+++ Week-2/Assignment 2.js
@@ -12,17 +12,17 @@ function calculate(args){
 
 // Method 1
 
-var keys = ['n1','n2','op'];
-var values = [10,4,'+'];
+// var keys = ['n1','n2','op'];
+// var values = [10,4,'+'];
 
-function toObject(keys, values) {
-    var result = {};
-    for (let i = 0; i < keys.length; i++)
-         result[keys[i]] = values[i];
-    return result;
-}
+// function toObject(keys, values) {
+//     var result = {};
+//     for (let i = 0; i < keys.length; i++)
+//          result[keys[i]] = values[i];
+//     return result;
+// }
 
-console.log(calculate(toObject(keys, values)));
+// console.log(calculate(toObject(keys, values)));
 
 
 // Method 2
@@ -31,14 +31,24 @@ console.log(calculate({n1 : 9, n2 : 7, op : '-'}));
 
 // Method 3
 
-var keys = ['n1','n2','op'];
-var values = [11,7,'+'];
+// var keys = ['n1','n2','op'];
+// var values = [11,7,'+'];
 
-function toObject(keys, values) {
-    let result = {};
-    for (let i = 0; i < keys.length; i++)
-         result[keys[i]] = values[i];
-    return result;
-}
+// function toObject(keys, values) {
+//     let result = {};
+//     for (let i = 0; i < keys.length; i++)
+//          result[keys[i]] = values[i];
+//     return result;
+// }
 
-console.log(calculate(toObject(keys, values)));
+// console.log(calculate(toObject(keys, values)));
+
+//Method 4
+function calData (n1,n2, op) {
+    this.n1 = n1;
+    this.n2 = n2;
+    this.op = op;
+} 
+
+console.log (calculate (new calData ( 6 , 1 , '-' ) ) ) ;
+
